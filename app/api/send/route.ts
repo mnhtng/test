88301955515848f658ed162ed797a29607f80220
Manh Tung <143700010+mnhtng@users.noreply.github.com>
2025-09-22 01:00:31 +0700

    three

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,13 +2,13 @@ import { sendPasswordResetEmail, sendVerificationMail } from "@/lib/resend";
 
 export async function GET() {
     await sendVerificationMail(
-        "domanhtung1404@gmail.com",
+        "domanhtung321@gmail.com",
         "http://localhost:3000/verify-email?token=1234",
         "MNHTNG"
     )
 
     await sendPasswordResetEmail(
-        "domanhtung1404@gmail.com",
+        "domanhtung321@gmail.com",
         "http://localhost:3000/reset-password?token=5678",
         "MNHTNG"
     )
