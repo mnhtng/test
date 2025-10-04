49e5576dd4fdf56d7b0d4e2295f896b2f999c321
Manh Tung <143700010+mnhtng@users.noreply.github.com>
2025-10-04 12:07:35 +0700

    Change email address for verification and reset emails

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,13 +2,13 @@ import { sendPasswordResetEmail, sendVerificationMail } from "@/lib/resend";
 
 export async function GET() {
     await sendVerificationMail(
-        "domanhtung321@gmail.com",
+        "quocvietdo4@gmail.com",
         "http://localhost:3000/verify-email?token=1234",
         "MNHTNG"
     )
 
     await sendPasswordResetEmail(
-        "domanhtung321@gmail.com",
+        "quocvietdo4@gmail.com",
         "http://localhost:3000/reset-password?token=5678",
         "MNHTNG"
     )
